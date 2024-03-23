3e1a2d4ed1734fd0451c3fbc4763a0978cecb9b9
caton-mark <100426775+The-Vijnana-Of-Coding@users.noreply.github.com>
2024-03-23 13:14:01 +0000

    LCD added DS18B20 Sensor Added Boot Screen Added

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,108 @@
 #include <Arduino.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define ONE_WIRE_BUS 5 // Pin connected to DS18B20 data line
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+int LCD_ADDR = 0x27;
+int LCD_COLS=  20;
+int LCD_ROWS = 4;
+
+LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
+
+enum START_ROW
+{
+	beginning,
+	middle,
+	end,
+	other
+};
+
+void LCDPrint(uint8_t line, uint8_t textStart, const char* text)
+{
+	uint8_t textLength = strlen(text);
+	uint8_t screenLength = LCD_COLS;
+	uint8_t startPosition = 0;
+
+	lcd.setCursor(0, line);
+	lcd.print("                    ");
+
+	if(textStart == beginning)
+	{
+		lcd.setCursor(0, line);
+    	lcd.print(text);
+	}
+	else if(textStart == middle)
+	{
+		// Calculate start position to center text
+		startPosition = (screenLength - textLength) / 2;
+		lcd.setCursor(startPosition, line);	
+    	lcd.print(text);
+	}
+	else if(textStart == end)
+	{
+		startPosition = screenLength - textLength;
+		lcd.setCursor(startPosition, line);	
+    	lcd.print(text);
+	}
+	else if(textStart == other)
+	{
+
+	}
+}
+
+void LCDBootScreen()
+{
+	LCDPrint(1, middle, "...Booting...");
+	LCDPrint(2, middle, "Please Wait");
+}
+
+void LCDStart()
+{
+	lcd.init();
+    lcd.backlight();
+    lcd.clear();
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Wire.begin();
+    Serial.begin(115200);
+
+	LCDStart();
+	LCDBootScreen();
+
+    sensors.begin(); // Initialize temperature sensor
+
+	delay(1000);
+    lcd.clear();
+}
+
+void SensorUpdate()
+{
+    sensors.requestTemperatures(); // Request temperature readings
+    float temperatureC = sensors.getTempCByIndex(0); // Read temperature in Celsius
+
+    // Convert float to string
+    String temperatureString = String(temperatureC, 1);
+
+    // Concatenate the string
+    String output = "Temp: " + temperatureString + " C";
+
+    // Convert concatenated string to char array
+    char charArray[output.length() + 1];
+    output.toCharArray(charArray, output.length() + 1);
+
+    // Print to LCD
+    LCDPrint(0, end, charArray);
+
+    delay(1000); // Delay before taking the next temperature reading
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	SensorUpdate();
+    delay(1000); // Delay before taking the next temperature reading
 }
